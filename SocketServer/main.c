04f694bd3ccf736af284502ca23324d6d9fd7816
Ayou <xiong_you_liang@sina.com>
2014-09-27 16:31:14 +0800

    Initial Commit

---
+++ SocketServer/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SocketServer
+//
+//  Created by happytree2 on 14-9-27.
+//  Copyright (c) 2014年 yellowpomelo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
